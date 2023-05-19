248df1d17020c4b121570021c5e6b24dd10b866d
Sinoce68 <126144473+Sinoce68@users.noreply.github.com>
2023-05-19 07:54:58 +0330

    Add files via upload

---
+++ set.py
@@ -0,0 +1,9 @@
+from os import system as sys
+from colorama import Fore as c
+from time import sleep as sp
+from random import choice as ch
+from random import randint as it
+import gauto_android as ga
+ga.init_logger()
+log = ga.logging
+log.shutdown()
